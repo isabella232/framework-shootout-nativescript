f784f16cd9a6bf8246ced6dcb06c4e9ba3b69c3a
Akash Agrawal <akagr@outlook.com>
2016-04-26 00:02:22 +0530

    Add visual indicator for non-zero recalls

--- app/views/description/description.js
+++ app/views/description/description.js
@@ -2,11 +2,21 @@ const NCAPModel = require('~/models/ncap');
 
 const ctx = NCAPModel.getEmptyPlaceholder();
 
+const colorRecallContainer = (page, recalls) => {
+    recalls = recalls || 0;
+    const view = page.getViewById('recallCard');
+    if (recalls > 0) {
+        view.className = view.className + ' color-warning';
+    }
+};
+
 exports.onNavigatingTo = args => {
     const page = args.object;
 
     const {vehicle} = page.navigationContext;
     ctx.set('vehicle', vehicle);
 
+    colorRecallContainer(page, vehicle.RecallsCount);
     page.bindingContext = ctx;
 };
+
